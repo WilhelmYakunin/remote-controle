dcc2b160d068e172f39b615ae6b81b054f33b7a7
WilhelmYakunin <v.jakunin@inbox.ru>
2023-05-22 20:07:12 +0300

    feat: add coverage for functions

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+const constants = {
+  MOUSE_POSITION: 'mouse_position',
+  PRINT_SCREEN: 'prnt_scrn',
+  PRINT_SCREN_SETTINGS: 'prnt_scrn base64 string (png buf)',
+  DRAW_CIRCLE: 'draw_circle',
+  DRAW_RECTANGLE: 'draw_rectangle',
+  DRAW_SQAURE: 'draw_square',
+  MOUSE_UP: 'mouse_up',
+  MOUSE_DOWN: 'mouse_down',
+  MOUSE_RIGHT: 'mouse_right',
+  MOUSE_LEFT: 'mouse_left',
+};
+
+export default constants;
